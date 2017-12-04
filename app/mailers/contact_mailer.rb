e926488c6c5985aa5e7d767c0d932ac82172792b
Hix OM Savis <hi.savis@gmail.com>
2017-12-04 14:53:56 +0000

    Configured contact mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,11 @@
 class ContactMailer < ActionMailer ::Base
   default to: 'hi.savis@gmail.com'
   
-  def 
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
     
+    mail(from: email, subject: 'Contact Form Message')
   end
 end
