9efb48c8772071acd45bcd071fefdd1ebed6e282
Hix OM Savis <hi.savis@gmail.com>
2017-12-13 19:13:22 +0000

    Added profiles table

--- config/environment.rb
+++ config/environment.rb
@@ -10,7 +10,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => ENV['infinite-dawn-44011.herokuapp.com'],
+  :domain         => 'infinite-dawn-44011.herokuapp.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
